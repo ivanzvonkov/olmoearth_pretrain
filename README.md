15f35d91f834e216c14e2aafe3b4955a34dcfe83
hgherzog <henryh@allenai.org>
2025-01-31 14:00:45 -0800

    update readme

--- README.md
+++ README.md
@@ -4,7 +4,9 @@ Highly Efficient Learning for Integrated Observation Systems (HELIOS)
 earth system foundation model data, training, and eval
 
 
-Setup
+Setup Instructions for running olmo_core_proto.py
 
-1. pip install -e .
-2. Log into gcloud
+1. Create a virtual environment in prefered directory with python 3.12 `python3 -m venv .venv-helios` \
+2. Navigate to root directory of this repo and run `pip install -e .`
+3. Clone the [Olmo-core](https://github.com/allenai/OLMo-core/tree/v2) repo and switch to the v2 branch
+4. Navigate to the root directory of olmo-core repository and run `pip install -e .`
--- helios/olmo_core_proto.py
+++ helios/olmo_core_proto.py
@@ -1,21 +1,12 @@
-"""Trying to prototype fitting everything into olmo core.
-
-TO run this script please clone the olmo-core repo and pip install the latest version of olmo-core.
-
-The released version on pypi is behind what is used here.
-
-"""
+"""Trying to prototype fitting everything into olmo core."""
 
 import logging
 import uuid
 
 import numpy as np
-from olmo_core.distributed.parallel import DataParallelConfig, DataParallelType
-from olmo_core.distributed.utils import (get_fs_local_rank, get_rank,
-                                         get_world_size)
+from olmo_core.distributed.utils import get_fs_local_rank, get_rank, get_world_size
 from olmo_core.optim import AdamWConfig
-from olmo_core.train import (prepare_training_environment,
-                             teardown_training_environment)
+from olmo_core.train import prepare_training_environment, teardown_training_environment
 from olmo_core.train.callbacks.wandb import WandBCallback
 from olmo_core.train.checkpoint import CheckpointerConfig
 from olmo_core.train.common import Duration, LoadStrategy
@@ -31,15 +22,23 @@ from helios.train.callbacks.speed_monitor import HeliosSpeedMonitorCallback
 from helios.train.decoder import SimpleLatentDecoder
 from helios.train.encoder import PatchEncoder
 from helios.train.loss import patch_disc_loss
+from helios.train.train_module import HeliosTrainModule
 from helios.train.trainer import HeliosTrainer
 
 logger = logging.getLogger(__name__)
 
 
-## Config does not yet support our new dataset type so we will construct manually for now
-
-
 if __name__ == "__main__":
+    # Variables to be changed per user
+    workdir = UPath("/Users/henryh/Desktop/eai-repos/helios-repos/helios/workdir")
+    WANDB_USERNAME = "henryhzog"
+    WANDB_PROJECT = "helios-test"
+
+    dp_config = None
+    # for distributed training use torchrun
+    # Uncomment this line to use distributed training
+    # dp_config = DataParallelConfig(name=DataParallelType.ddp)
+
     # for distributed training use torchrun
     prepare_training_environment(seed=42)
     # set log level to debug
@@ -48,8 +47,6 @@ if __name__ == "__main__":
     index_path = "/weka/dfive-default/helios_sample_data/20250115-sample-dataset-helios/index.csv"
     index_parser = DatasetIndexParser(index_path)
     samples = index_parser.samples
-    workdir = UPath("/Users/henryh/Desktop/eai-repos/helios-repos/helios/workdir")
-
 
     # Variable masking is not used
     encoder = PatchEncoder(
@@ -67,7 +64,6 @@ if __name__ == "__main__":
     )
     model = LatentMIMStyle(encoder, decoder)
 
-
     max_duration = Duration.epochs(4)
     device = get_default_device()
     # Ideally though this should be handled by the Model COnfig and build
@@ -75,9 +71,7 @@ if __name__ == "__main__":
     checkpointer_config = CheckpointerConfig(work_dir=workdir)
     checkpointer = checkpointer_config.build()
     optim_config = AdamWConfig()
-    from helios.train.train_module import HeliosTrainModule
 
-    dp_config = DataParallelConfig(name=DataParallelType.ddp)
     train_module = HeliosTrainModule(
         model=model,
         optim=optim_config,
@@ -105,8 +99,8 @@ if __name__ == "__main__":
     run_name = f"test-debug-{str(uuid.uuid4())[:8]}"
     wandb_callback = WandBCallback(
         name=run_name,
-        project="helios-test",
-        entity="henryhzog",  # PLEASE CHANGE
+        project=WANDB_PROJECT,
+        entity=WANDB_USERNAME,
     )
     trainer = HeliosTrainer(
         work_dir=workdir,
