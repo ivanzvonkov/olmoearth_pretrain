b39798f10485b1b56195edc577368d3bc9b45086
Favyen Bastani <favyenb@allenai.org>
2025-02-06 09:41:56 -0800

    remove unused MODALITIES

--- helios/dataset_creation/constants.py
+++ helios/dataset_creation/constants.py
@@ -2,19 +2,6 @@
 
 from rslearn.utils.raster_format import GeotiffRasterFormat
 
-MODALITIES = [
-    "landsat_freq",
-    "landsat_monthly",
-    "naip",
-    "openstreetmap",
-    "openstreetmap_raster",
-    "sentinel1_freq",
-    "sentinel1_monthly",
-    "sentinel2_freq",
-    "sentinel2_monthly",
-    "worldcover",
-]
-
 # Columns in the per-modality metadata CSVs.
 METADATA_COLUMNS = [
     "crs",
