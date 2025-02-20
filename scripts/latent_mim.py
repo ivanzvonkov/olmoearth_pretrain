d2cbb1e269f3821cfe05767897ac94236b7065b4
hgherzog <henryh@allenai.org>
2025-02-19 18:12:57 -0800

    make model size configurable

--- scripts/latent_mim.py
+++ scripts/latent_mim.py
@@ -5,17 +5,6 @@ import uuid
 from os import environ
 
 import numpy as np
-from olmo_core.distributed.utils import get_fs_local_rank, get_rank, get_world_size
-from olmo_core.optim import AdamWConfig
-from olmo_core.optim.scheduler import ConstantWithWarmup
-from olmo_core.train import prepare_training_environment, teardown_training_environment
-from olmo_core.train.callbacks import GPUMemoryMonitorCallback, WandBCallback
-from olmo_core.train.checkpoint import CheckpointerConfig
-from olmo_core.train.common import Duration, LoadStrategy
-from olmo_core.train.config import TrainerConfig
-from olmo_core.utils import get_default_device
-from upath import UPath
-
 from helios.data.constants import Modality
 from helios.data.dataloader import HeliosDataLoaderConfig
 from helios.data.dataset import HeliosDatasetConfig, collate_helios
@@ -25,6 +14,18 @@ from helios.train.callbacks.speed_monitor import HeliosSpeedMonitorCallback
 from helios.train.loss import LossConfig
 from helios.train.masking import MaskingConfig
 from helios.train.train_module.latent_mim import LatentMIMTrainModuleConfig
+from olmo_core.distributed.utils import (get_fs_local_rank, get_rank,
+                                         get_world_size)
+from olmo_core.optim import AdamWConfig
+from olmo_core.optim.scheduler import ConstantWithWarmup
+from olmo_core.train import (prepare_training_environment,
+                             teardown_training_environment)
+from olmo_core.train.callbacks import GPUMemoryMonitorCallback, WandBCallback
+from olmo_core.train.checkpoint import CheckpointerConfig
+from olmo_core.train.common import Duration, LoadStrategy
+from olmo_core.train.config import TrainerConfig
+from olmo_core.utils import get_default_device
+from upath import UPath
 
 logger = logging.getLogger(__name__)
 
@@ -38,13 +39,13 @@ if __name__ == "__main__":
 
     WANDB_USERNAME = "eai-ai2"  # nosec
     WANDB_PROJECT = "helios-debug"
-    run_name = f"helios-beaker-test-pre-empt-{str(uuid.uuid4())[:8]}"
+    run_name = f"helios-test-actual-model-size-{str(uuid.uuid4())[:8]}"
     # PER EXPERIMENT Variables
     LR = 0.0001
-    GLOBAL_BATCH_SIZE = 32
+    GLOBAL_BATCH_SIZE = 64
     RANK_BATCH_SIZE = 32
     MAX_DURATION = Duration.epochs(50)
-    NUM_WORKERS = 16
+    NUM_WORKERS = 4
     NUM_THREADS = 0
     METRICS_COLLECT_INTERVAL = 1
     CANCEL_CHECK_INTERVAL = 1
@@ -57,7 +58,7 @@ if __name__ == "__main__":
         Modality.SENTINEL2,
         Modality.LATLON,
         Modality.SENTINEL1,
-        # Modality.WORLDCOVER,
+        Modality.WORLDCOVER,
     ]
     MAX_PATCH_SIZE = 8  # NOTE: actual patch_size <= max_patch_size
     ENCODE_RATIO = 0.5
@@ -65,7 +66,14 @@ if __name__ == "__main__":
     TOKEN_BUDGET = 1500
     H_W_TO_SAMPLE_MIN = 2
     H_W_TO_SAMPLE_MAX = 13
-    WARMUP_STEPS = 10
+    WARMUP_STEPS = 2
+    ENCODER_EMBEDDING_SIZE = 128
+    DECODER_EMBEDDING_SIZE = 128
+    ENCODER_DEPTH = 4
+    DECODER_DEPTH = 4
+    ENCODER_NUM_HEADS = 8
+    DECODER_NUM_HEADS = 8
+    MLP_RATIO = 4.0
     #################### Setup environment ####################
     dp_config = None
     # for distributed training use torchrun
@@ -83,21 +91,21 @@ if __name__ == "__main__":
     # TODO: build encoder_small, encoder_base, encoder_large, etc. Same for decoder
     encoder_config = EncoderConfig(
         supported_modalities=SUPPORTED_MODALITIES,
-        embedding_size=16,
+        embedding_size=ENCODER_EMBEDDING_SIZE,
         max_patch_size=MAX_PATCH_SIZE,
-        num_heads=2,
-        depth=2,
-        mlp_ratio=1.0,
+        num_heads=ENCODER_NUM_HEADS,
+        depth=ENCODER_DEPTH,
+        mlp_ratio=MLP_RATIO,
         drop_path=0.1,
         max_sequence_length=12,
         use_channel_embs=True,
     )
     decoder_config = PredictorConfig(
-        encoder_embedding_size=16,
-        decoder_embedding_size=16,
-        depth=2,
-        mlp_ratio=1.0,
-        num_heads=2,
+        encoder_embedding_size=ENCODER_EMBEDDING_SIZE,
+        decoder_embedding_size=DECODER_EMBEDDING_SIZE,
+        depth=DECODER_DEPTH,
+        mlp_ratio=MLP_RATIO,
+        num_heads=DECODER_NUM_HEADS,
         max_sequence_length=12,
         supported_modalities=SUPPORTED_MODALITIES,
     )
@@ -196,11 +204,10 @@ if __name__ == "__main__":
     #################### Eval ####################
     # eval. Currently this will fail because by default our model ingests 4 timesteps.
     # we should update the model architecture to ingest variable numbers of timesteps
-    from torch.utils.data import DataLoader
-
     from helios.evals.datasets import GeobenchDataset
     from helios.evals.embeddings import get_embeddings
     from helios.evals.knn import run_knn
+    from torch.utils.data import DataLoader
 
     geobench_dir = UPath("/weka/skylight-default/presto-geobench/dataset/geobench")
 
