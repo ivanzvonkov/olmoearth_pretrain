9cf0b03d7a23f72c44a139555921f20827b8947a
hgherzog <henryh@allenai.org>
2025-02-21 12:46:50 -0800

    liint

--- helios/nn/utils.py
+++ helios/nn/utils.py
@@ -1,5 +1,6 @@
 """Utilities for the nn module."""
 
+import torch
 from torch.distributed import DeviceMesh
 
 
@@ -11,9 +12,8 @@ class DistributedMixins:
         dp_mesh: DeviceMesh | None = None,
         compile_enabled: bool = False,
         autograd_compile_enabled: bool = False,
-    ):
-        """
-        Apply DDP to the model.
+    ) -> None:
+        """Apply DDP to the model.
 
         .. warning::
             Usually this does not need to be called directly, as :meth:`TransformerConfig.build()`
@@ -25,7 +25,9 @@ class DistributedMixins:
         # https://github.com/pytorch/torchtitan/blob/90c889e972b56b9faadebbb78fc985dedc537ed9/torchtitan/parallelisms/parallelize_llama.py#L328
         if compile_enabled:
             if autograd_compile_enabled:
-                torch._dynamo.config.optimize_ddp = "python_reducer_without_compiled_forward"  # type: ignore
+                torch._dynamo.config.optimize_ddp = (
+                    "python_reducer_without_compiled_forward"  # type: ignore
+                )
             else:
                 torch._dynamo.config.optimize_ddp = "ddp_optimizer"  # type: ignore
 
