232611bd49f0267d7432112308d8f13dcda950d6
hgherzog <henryh@allenai.org>
2025-02-24 09:03:01 -0800

    wip writing tests

--- tests/unit/data/test_visualize.py
+++ tests/unit/data/test_visualize.py
@@ -6,10 +6,8 @@ from helios.data.dataset import HeliosDataset
 from helios.data.visualize import visualize_sample
 
 
-
-
-
 def test_visualize_sample():
     """Test the visualize_sample function."""
+    samples = []
     dataset = HeliosDataset()
     visualize_sample(dataset, 0)
