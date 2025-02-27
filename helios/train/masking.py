0789e143bf893a2776bfd83b66f695804f23fe4d
Joseph Redmon <pjreddie@gmail.com>
2025-02-27 10:19:25 -0800

    space structured masking and tests

--- helios/train/masking.py
+++ helios/train/masking.py
@@ -9,7 +9,7 @@ from typing import Any, NamedTuple
 import numpy as np
 import torch
 from class_registry import ClassRegistry
-from einops import repeat
+from einops import rearrange, repeat
 from olmo_core.config import Config
 
 from helios.data.constants import Modality, ModalitySpec
@@ -207,6 +207,118 @@ MASKING_STRATEGY_REGISTRY = ClassRegistry[MaskingStrategy]()
 
 
 # EXAMPLE
+@MASKING_STRATEGY_REGISTRY.register("space")
+class SpaceMaskingStrategy(MaskingStrategy):
+    """Spatially structured random masking of the input data."""
+
+    def __init__(
+        self,
+        encode_ratio: float = 0.5,
+        decode_ratio: float = 0.5,
+    ) -> None:
+        """Initialize the masking strategy."""
+        self._encode_ratio = encode_ratio
+        self._decode_ratio = decode_ratio
+        self.generator = np.random.default_rng(0)
+
+    def _create_mask_per_modality(
+        self,
+        modality: ModalitySpec,
+        shape: torch.Size,
+        patch_size: int,
+        device: torch.device | None = None,
+    ) -> ArrayTensor:
+        if not modality.is_spatial:
+            raise ValueError("Non-spatial modality {modality}")
+
+        b, h, w = shape[:3]
+        t = shape[3] if modality.is_multitemporal else 1
+        b_s = shape[-1]
+
+        assert (h % patch_size == 0) and (w % patch_size == 0)
+        h_p = h // patch_size
+        w_p = w // patch_size
+
+        patches = h_p * w_p
+        encode_patches = int(self.encode_ratio * patches)
+        decode_patches = int(self.decode_ratio * patches)
+        target_patches = patches - encode_patches - decode_patches
+
+        flat_mask = np.concatenate(
+            (
+                np.ones(target_patches, dtype=np.int_),
+                np.ones(decode_patches, dtype=np.int_) * 2,
+                np.zeros(encode_patches, dtype=np.int_),
+            )
+        )
+
+        # numpy to for permuted function
+        flat_mask = repeat(flat_mask, "x -> b x", b=b)
+        flat_mask = self.generator.permuted(flat_mask, axis=1)
+        patch_mask = rearrange(flat_mask, "b (h w) -> b h w", h=h_p, w=w_p)
+
+        mask = np.repeat(patch_mask, repeats=patch_size, axis=1)
+        mask = np.repeat(mask, repeats=patch_size, axis=2)
+        mask = repeat(mask, "... -> ... t b_s", t=t, b_s=b_s)
+        mask = torch.as_tensor(mask, device=device)
+        mask = mask.view(*shape)
+        return mask
+
+    def apply_mask(
+        self, batch: HeliosSample, patch_size: int = 1, **kwargs: Any
+    ) -> MaskedHeliosSample:
+        """Apply random masking to the input data.
+
+        All Masking happens in unpatchified form and not grouped across bandsets
+        as the modality data is unpatchified and not grouped across bandsets
+
+        The mask created for the space-time varying modality will be different than
+        for the static modality.
+
+        For space-time varying data, we will mask out the same ratio of values for
+        all the instances in the batch. However, since a static modality might have
+        very few tokens in a batch (e.g. 1 for latlons) instead we mask out a certain
+        ratios of values across the entire batch.
+
+        Args:
+            batch: Input data of type HeliosSample
+            patch_size: patch size applied to sample
+            **kwargs: Additional arguments for maskings
+
+        Returns:
+            MaskedHeliosSample containing the masked data and mask
+        """
+        output_dict: dict[str, ArrayTensor | None] = {}
+        for modality_name in batch._fields:
+            modality = getattr(batch, modality_name)
+            if modality is None:
+                # set modality and mask to None
+                output_dict[modality_name] = None
+                output_dict[
+                    MaskedHeliosSample.get_masked_modality_name(modality_name)
+                ] = None
+            else:
+                if modality_name == "timestamps":
+                    output_dict[modality_name] = modality
+                    continue
+
+                if isinstance(modality, torch.Tensor):
+                    device: torch.device | None = modality.device
+                else:
+                    device = None
+
+                mask = self._create_mask_per_modality(
+                    Modality.get(modality_name), modality.shape, patch_size, device
+                )
+
+                output_dict[modality_name] = modality
+                output_dict[
+                    MaskedHeliosSample.get_masked_modality_name(modality_name)
+                ] = mask
+        return MaskedHeliosSample(**output_dict)
+
+
+# EXAMPLE
 @MASKING_STRATEGY_REGISTRY.register("random")
 class RandomMaskingStrategy(MaskingStrategy):
     """Randomly masks the input data."""
@@ -225,13 +337,13 @@ class RandomMaskingStrategy(MaskingStrategy):
         self,
         modality: ModalitySpec,
         shape: torch.Size,
-        return_device: torch.device | None = None,
+        device: torch.device | None = None,
     ) -> ArrayTensor:
         if modality.is_spatial or modality.is_multitemporal:
             b = shape[0]
-            num_tokens_per_instance = np.prod(shape[1:-1]) * modality.num_bands
+            num_tokens_per_instance = np.prod(shape[1:])
         else:
-            num_tokens_per_instance = np.prod(shape[:-1]) * modality.num_bands
+            num_tokens_per_instance = np.prod(shape)
         num_encode_tokens = int(num_tokens_per_instance * self.encode_ratio)
         num_decode_tokens = int(num_tokens_per_instance * self.decode_ratio)
         num_target_encode_tokens = int(
@@ -253,8 +365,8 @@ class RandomMaskingStrategy(MaskingStrategy):
         else:
             flat_mask_tokens = self.generator.permuted(flat_mask_tokens)
 
-        mask = torch.as_tensor(flat_mask_tokens, device=return_device)
-        mask = mask.view(*shape[:-1], modality.num_bands)
+        mask = torch.as_tensor(flat_mask_tokens, device=device)
+        mask = mask.view(*shape)
         return mask
 
     def apply_mask(self, batch: HeliosSample, **kwargs: Any) -> MaskedHeliosSample:
@@ -293,12 +405,12 @@ class RandomMaskingStrategy(MaskingStrategy):
                     continue
 
                 if isinstance(modality, torch.Tensor):
-                    return_device: torch.device | None = modality.device
+                    device: torch.device | None = modality.device
                 else:
-                    return_device = None
+                    device = None
 
                 mask = self._create_mask_per_modality(
-                    Modality.get(modality_name), modality.shape, return_device
+                    Modality.get(modality_name), modality.shape, device
                 )
 
                 output_dict[modality_name] = modality
--- tests/unit/train/test_masking.py
+++ tests/unit/train/test_masking.py
@@ -6,7 +6,7 @@ import torch
 
 from helios.data.constants import Modality
 from helios.data.dataset import HeliosSample
-from helios.train.masking import MaskValue, RandomMaskingStrategy
+from helios.train.masking import MaskValue, RandomMaskingStrategy, SpaceMaskingStrategy
 
 logger = logging.getLogger(__name__)
 
@@ -20,11 +20,13 @@ def test_random_masking_and_unmask() -> None:
     years = torch.randint(2018, 2020, (b, 1, t), dtype=torch.long)
     timestamps = torch.cat([days, months, years], dim=1)  # Shape: (B, 3, T)
     sentinel2_l2a_num_bands = Modality.SENTINEL2_L2A.num_bands
+    worldcover_num_bands = Modality.WORLDCOVER.num_bands
     latlon_num_bands = Modality.LATLON.num_bands
     batch = HeliosSample(
         sentinel2_l2a=torch.ones((b, h, w, t, sentinel2_l2a_num_bands)),
         latlon=torch.ones((b, latlon_num_bands)),
         timestamps=timestamps,
+        worldcover=torch.ones((b, h, w, worldcover_num_bands)),
     )
     encode_ratio, decode_ratio = 0.25, 0.5
     masked_sample = RandomMaskingStrategy(
@@ -32,6 +34,61 @@ def test_random_masking_and_unmask() -> None:
         decode_ratio=decode_ratio,
     ).apply_mask(
         batch,
+    )
+    # check that each modality has the right masking ratio
+    for modality_name in masked_sample._fields:
+        if modality_name.endswith("mask"):
+            mask = getattr(masked_sample, modality_name)
+            logger.info(f"Mask name: {modality_name}")
+            if mask is None:
+                continue
+            total_elements = mask.numel()
+            num_encoder = len(mask[mask == MaskValue.ONLINE_ENCODER.value])
+            num_decoder = len(mask[mask == MaskValue.DECODER.value])
+            assert (
+                num_encoder / total_elements
+            ) == encode_ratio, f"{modality_name} has incorrect encode mask ratio"
+            assert (
+                num_decoder / total_elements
+            ) == decode_ratio, f"{modality_name} has incorrect decode mask ratio"
+            assert (
+                mask.shape
+                == getattr(
+                    batch,
+                    masked_sample.get_unmasked_modality_name(modality_name),
+                ).shape
+            ), f"{modality_name} has incorrect shape"
+
+    unmasked_sample = masked_sample.unmask()
+    for modality_name in unmasked_sample._fields:
+        if modality_name.endswith("mask"):
+            mask = getattr(unmasked_sample, modality_name)
+            if mask is not None:
+                assert (mask == 0).all()
+
+
+def test_space_structure_masking_and_unmask() -> None:
+    """Test space structure masking ratios."""
+    b, h, w, t = 100, 16, 16, 8
+
+    days = torch.randint(1, 31, (b, 1, t), dtype=torch.long)
+    months = torch.randint(1, 13, (b, 1, t), dtype=torch.long)
+    years = torch.randint(2018, 2020, (b, 1, t), dtype=torch.long)
+    timestamps = torch.cat([days, months, years], dim=1)  # Shape: (B, 3, T)
+    sentinel2_l2a_num_bands = Modality.SENTINEL2_L2A.num_bands
+    worldcover_num_bands = Modality.WORLDCOVER.num_bands
+    batch = HeliosSample(
+        sentinel2_l2a=torch.ones((b, h, w, t, sentinel2_l2a_num_bands)),
+        timestamps=timestamps,
+        worldcover=torch.ones((b, h, w, worldcover_num_bands)),
+    )
+    encode_ratio, decode_ratio = 0.25, 0.5
+    masked_sample = SpaceMaskingStrategy(
+        encode_ratio=encode_ratio,
+        decode_ratio=decode_ratio,
+    ).apply_mask(
+        batch,
+        patch_size=4,
     )
     # check that each modality has the right masking ratio
     for modality_name in masked_sample._fields:
