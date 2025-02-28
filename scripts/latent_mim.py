2067179d31c311c43470ebaa095eb8cd2fae258c
hgherzog <henryh@allenai.org>
2025-02-28 12:42:26 -0800

    increase default learning rate

--- scripts/latent_mim.py
+++ scripts/latent_mim.py
@@ -96,7 +96,7 @@ def build_train_module_config(
     common: CommonComponents,
 ) -> LatentMIMTrainModuleConfig:
     """Build the train module config for an experiment."""
-    LR = 0.0001
+    LR = 0.002
     RANK_MICROBATCH_SIZE = 8
     ENCODE_RATIO = 0.1
     DECODE_RATIO = 0.75
