1f9b2a20929cc317a8d2655f88d1f16cc8cf2651
Yawen Zhang <yawenz@allenai.org>
2025-03-07 13:28:57 -0800

    rename to global

--- scripts/parameter_sweeping/20250307_galileo_global_tiny.py
+++ scripts/parameter_sweeping/20250307_galileo_global_tiny.py
@@ -54,7 +54,7 @@ BASE_COMMAND = (
 # Iterate over all combinations of hyperparameters
 for lr, wd, warmup in itertools.product(LEARNING_RATES, WEIGHT_DECAYS, WARMUP_EPOCHS):
     # Construct run name indicating hyperparameters
-    run_name = f"galileo_local_tiny_lr_{lr}_wd_{wd}_warmup_{warmup}"
+    run_name = f"galileo_global_tiny_lr_{lr}_wd_{wd}_warmup_{warmup}"
 
     # Construct full command
     command = BASE_COMMAND.format(
