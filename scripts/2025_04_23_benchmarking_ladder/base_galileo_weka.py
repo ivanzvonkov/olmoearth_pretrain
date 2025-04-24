9460987b0e8db10f72ca629cd696eba02bd69ec7
hgherzog <henryh@allenai.org>
2025-04-24 11:03:28 -0700

    put in a seperate project

--- scripts/2025_04_23_benchmarking_ladder/base_galileo_weka.py
+++ scripts/2025_04_23_benchmarking_ladder/base_galileo_weka.py
@@ -212,7 +212,7 @@ def build_trainer_config(common: CommonComponents) -> TrainerConfig:
     CANCEL_CHECK_INTERVAL = 25  # should be turned off for final run
     LOAD_STRATEGY = LoadStrategy.if_available
     WANDB_USERNAME = "eai-ai2"  # nosec
-    WANDB_PROJECT = "2025-04-23-galileo-contrastive-ladder"
+    WANDB_PROJECT = "2025-04-23-gbs-sweep"
     checkpointer_config = CheckpointerConfig(work_dir=common.save_folder)
     wandb_callback = HeliosWandBCallback(
         name=common.run_name,
