ef04800038e078fe9887d463862ef03e92277b87
hgherzog <henryh@allenai.org>
2025-04-28 16:43:59 -0700

    undo removing integration tests

---
+++ tests/integration/data/test_dataloader.py
@@ -0,0 +1,54 @@
+"""Test the HeliosDataloader class."""
+
+from pathlib import Path
+
+from helios.data.constants import Modality
+from helios.data.dataloader import HeliosDataLoader
+from helios.data.dataset import HeliosDataset, collate_helios
+
+
+def test_helios_dataloader(tmp_path: Path, setup_h5py_dir: Path) -> None:
+    """Test the HeliosDataloader class."""
+    training_modalities = [
+        Modality.SENTINEL2_L2A.name,
+        Modality.SENTINEL1.name,
+        Modality.WORLDCOVER.name,
+        Modality.OPENSTREETMAP_RASTER.name,
+    ]
+    dataset = HeliosDataset(
+        h5py_dir=setup_h5py_dir,
+        training_modalities=training_modalities,
+        dtype="float32",
+    )
+
+    dataset.prepare()
+    assert isinstance(dataset, HeliosDataset)
+    dataloader = HeliosDataLoader(
+        dataset=dataset,
+        work_dir=tmp_path,
+        global_batch_size=1,
+        dp_world_size=1,
+        dp_rank=0,
+        fs_local_rank=0,
+        seed=0,
+        shuffle=True,
+        num_workers=0,
+        collator=collate_helios,
+        target_device_type="cpu",
+        token_budget=1000000,
+        min_patch_size=1,
+        max_patch_size=1,
+        sampled_hw_p_list=[256],
+    )
+
+    dataloader.reshuffle()
+    batches_processed = 0
+    for batch in dataloader:
+        batches_processed += 1
+
+    state_dict = dataloader.state_dict()
+    dataloader.reset()
+    dataloader.load_state_dict(state_dict)
+    assert dataloader.batches_processed == batches_processed
+
+    assert batches_processed == 1
