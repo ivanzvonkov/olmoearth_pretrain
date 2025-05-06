0d04d2c819ffab40a6171ca8a3eef72447d0ace9
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2025-05-06 22:47:46 +0800

    Another go

--- scripts/base_debug_scripts/contrastive_latentmim.py
+++ scripts/base_debug_scripts/contrastive_latentmim.py
@@ -152,7 +152,7 @@ def build_dataloader_config(common: CommonComponents) -> HeliosDataLoaderConfig:
 def build_dataset_config(common: CommonComponents) -> HeliosDatasetConfig:
     """Build the dataset config for an experiment."""
     # NOTE: Change this directory based on the supported modalities
-    h5py_dir = "/weka/dfive-default/helios/dataset/presto/h5py_data/latlon_sentinel1_sentinel2_l2a_worldcover/98856"
+    h5py_dir = "/weka/dfive-default/helios/dataset/presto/h5py_data/sentinel1_sentinel2_l2a_worldcover/116711"
     return HeliosDatasetConfig(
         h5py_dir=h5py_dir,
         use_samples_with_missing_supported_modalities=True,
