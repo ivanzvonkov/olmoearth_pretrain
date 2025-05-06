436e16ca10e42af04767df93a7dd662430e26c8a
Joseph Redmon <pjreddie@gmail.com>
2025-05-06 15:58:37 -0700

    going back to try nano

--- scripts/base_debug_scripts/latent_mim.py
+++ scripts/base_debug_scripts/latent_mim.py
@@ -43,12 +43,13 @@ MIN_PATCH_SIZE = 1
 
 def build_model_config(common: CommonComponents) -> LatentMIMConfig:
     """Build the model config for an experiment."""
-    ENCODER_EMBEDDING_SIZE = 192
-    DECODER_EMBEDDING_SIZE = 192
-    ENCODER_DEPTH = 12
-    DECODER_DEPTH = 12
-    ENCODER_NUM_HEADS = 3
-    DECODER_NUM_HEADS = 3
+    ENCODER_EMBEDDING_SIZE = 128
+    DECODER_EMBEDDING_SIZE = 128
+    ENCODER_DEPTH = 4
+    DECODER_DEPTH = 4
+    ENCODER_NUM_HEADS = 8
+    DECODER_NUM_HEADS = 8
+
     MLP_RATIO = 4.0
     encoder_config = EncoderConfig(
         supported_modality_names=common.training_modalities,
