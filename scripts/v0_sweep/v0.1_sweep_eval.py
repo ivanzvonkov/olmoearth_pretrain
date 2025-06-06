9b06e2f1b4e186b1dd5715a3cdda22ff8eec85dd
Yawen Zhang <yawenz@allenai.org>
2025-06-05 17:56:43 -0700

    test run again

--- scripts/v0_sweep/v0.1_sweep_eval.py
+++ scripts/v0_sweep/v0.1_sweep_eval.py
@@ -2,7 +2,7 @@
 
 import subprocess  # nosec
 
-LP_LRs = [1e-4, 5e-4, 1e-3, 5e-3, 1e-2, 5e-2, 1e-1, 5e-1]
+LP_LRs = [1e-4]  # , 5e-4, 1e-3, 5e-3, 1e-2, 5e-2, 1e-1, 5e-1]
 
 # v0.1_base_latent_mim_contrastive_random
 base_cmd = (
