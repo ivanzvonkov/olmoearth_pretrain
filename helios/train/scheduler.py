f517e707ca95742ccf6aa2ad6b8940086adbb011
Joseph Redmon <pjreddie@gmail.com>
2025-06-11 15:10:29 -0700

    forgot we use old olmo

--- helios/train/scheduler.py
+++ helios/train/scheduler.py
@@ -3,43 +3,35 @@
 from dataclasses import dataclass
 
 import torch
-from olmo_core.exceptions import OLMoConfigurationError
 from olmo_core.optim.scheduler import Scheduler, _linear_warmup
+
+# TODO this may break if we upgrade olmo core they change scheduler behavior
 
 
 @dataclass
 class PolyWithWarmup(Scheduler):
     """Polynomial learning rate schedule with a warmup."""
 
-    warmup: int | None = None
+    warmup_steps: int | None = None
     warmup_fraction: float | None = None
     power: float = 3.3219
     t_max: int | None = None
     warmup_min_lr: float = 0.0
 
-    def __post_init__(self) -> None:
-        """Set up warmups."""
-        if (self.warmup_fraction is None) == (self.warmup is None):
-            raise OLMoConfigurationError(
-                "Either 'warmup_fraction' or 'warmup' must be specified."
-            )
-
-        if self.warmup_fraction is not None and (
-            self.warmup_fraction < 0 or self.warmup_fraction > 1
-        ):
-            raise OLMoConfigurationError("warmup_fraction must be between 0 and 1.")
-
     def get_lr(
-        self, initial_lr: float | torch.Tensor, current: int, t_max: int
+        self, initial_lr: float | torch.Tensor, step: int, max_steps: int
     ) -> float | torch.Tensor:
         """Get learning rate."""
+        t_max = max_steps
+        current = step
+
         t_max = t_max if self.t_max is None else self.t_max
 
-        if self.warmup is None:
+        if self.warmup_steps is None:
             assert self.warmup_fraction is not None
             warmup = round(t_max * self.warmup_fraction)
         else:
-            warmup = self.warmup
+            warmup = self.warmup_steps
 
         if current < warmup:
             return _linear_warmup(initial_lr, current, warmup, self.warmup_min_lr)
