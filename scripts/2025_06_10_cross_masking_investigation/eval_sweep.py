ef3edb5b0f3802a57311921d3c32fce9fa2004dd
hgherzog <henryh@allenai.org>
2025-06-26 15:43:28 -0700

    ready to laucnh on titan

--- scripts/2025_06_10_cross_masking_investigation/eval_sweep.py
+++ scripts/2025_06_10_cross_masking_investigation/eval_sweep.py
@@ -1,30 +1,36 @@
 import subprocess
+
 LP_LRs = [1e-4, 5e-4, 1e-3, 5e-3, 1e-2, 5e-2, 1e-1, 5e-1]
 
-lr_args = " ".join([
-    "--trainer.callbacks.downstream_evaluator.tasks.m-cashew-plant.probe_lr={lr}",
-    "--trainer.callbacks.downstream_evaluator.tasks.mados.probe_lr={lr}",
-    "--trainer.callbacks.downstream_evaluator.tasks.sen1floods11.probe_lr={lr}",
-    "--trainer.callbacks.downstream_evaluator.tasks.pastis_sentinel2.probe_lr={lr}",
-    "--trainer.callbacks.downstream_evaluator.tasks.pastis_sentinel1.probe_lr={lr}",
-    "--trainer.callbacks.downstream_evaluator.tasks.pastis_sentinel1_sentinel2.probe_lr={lr}",
-    "--trainer.callbacks.downstream_evaluator.tasks.sickle_sentinel1.probe_lr={lr}",
-    "--trainer.callbacks.downstream_evaluator.tasks.sickle_landsat.probe_lr={lr}",
-    "--trainer.callbacks.downstream_evaluator.tasks.sickle_sentinel1_landsat.probe_lr={lr}",
-    "--trainer.callbacks.downstream_evaluator.tasks.breizhcrops.probe_lr={lr}",
-])
-checkpoints = ["/weka/dfive-default/helios/checkpoints/henryh/v0.2_latent_mim_128_cross_random_allow_overlap_minenc_2_alldec_only_dec_srtm_worldcover_osm_mask_0.5/step185000",
- "/weka/dfive-default/helios/checkpoints/henryh/v0.2_latent_mim_128_cross_random_allow_overlap_minenc_2_alldec_only_dec_srtm_worldcover_osm_mask_0.5/step160000",
-  "/weka/dfive-default/helios/checkpoints/henryh/v0.2_latent_mim_128_cross_random_allow_overlap_minenc_2_alldec_only_dec_srtm_worldcover_osm_mask_0.5/step235950"
+lr_args = " ".join(
+    [
+        "--trainer.callbacks.downstream_evaluator.tasks.m-cashew-plant.probe_lr={lr}",
+        "--trainer.callbacks.downstream_evaluator.tasks.mados.probe_lr={lr}",
+        "--trainer.callbacks.downstream_evaluator.tasks.sen1floods11.probe_lr={lr}",
+        "--trainer.callbacks.downstream_evaluator.tasks.pastis_sentinel2.probe_lr={lr}",
+        "--trainer.callbacks.downstream_evaluator.tasks.pastis_sentinel1.probe_lr={lr}",
+        "--trainer.callbacks.downstream_evaluator.tasks.pastis_sentinel1_sentinel2.probe_lr={lr}",
+        "--trainer.callbacks.downstream_evaluator.tasks.sickle_sentinel1.probe_lr={lr}",
+        "--trainer.callbacks.downstream_evaluator.tasks.sickle_landsat.probe_lr={lr}",
+        "--trainer.callbacks.downstream_evaluator.tasks.sickle_sentinel1_landsat.probe_lr={lr}",
+        "--trainer.callbacks.downstream_evaluator.tasks.breizhcrops.probe_lr={lr}",
+    ]
+)
+checkpoints = [
+    "/weka/dfive-default/helios/checkpoints/henryh/v0.2_latent_mim_128_cross_random_allow_overlap_minenc_2_alldec_only_dec_srtm_worldcover_osm_mask_0.5/step185000",
+    "/weka/dfive-default/helios/checkpoints/henryh/v0.2_latent_mim_128_cross_random_allow_overlap_minenc_2_alldec_only_dec_srtm_worldcover_osm_mask_0.5/step160000",
+    "/weka/dfive-default/helios/checkpoints/henryh/v0.2_latent_mim_128_cross_random_allow_overlap_minenc_2_alldec_only_dec_srtm_worldcover_osm_mask_0.5/step235950",
 ]
-cluster= "local" #"ai2/titan-cirrascale"
-run_cmd = "train" #"launch"
+cluster = "ai2/titan-cirrascale"
+run_cmd = "launch"
 for checkpoint in checkpoints:
     for probe_lr in LP_LRs:
-        run_name = checkpoint.split("/")[-2] + checkpoint.split("/")[-1] + f"_eval_{probe_lr}"
+        run_name = (
+            checkpoint.split("/")[-2] + checkpoint.split("/")[-1] + f"_eval_{probe_lr}"
+        )
         start_command = "python3" if run_cmd == "launch" else "torchrun"
         lr_args = lr_args.format(lr=probe_lr)
         print(lr_args)
-        cmd = f"{start_command} scripts/2025_06_10_cross_masking_investigation/eval.py {run_cmd} {run_name} {cluster} {lr_args} --trainer.load_path={checkpoint}"
+        cmd = f"{start_command} scripts/2025_06_10_cross_masking_investigation/eval.py {run_cmd} {run_name} {cluster} --launch.priority=high {lr_args} --trainer.load_path={checkpoint}"
         print(cmd)
-        subprocess.run(cmd, shell=True)
+        subprocess.run(cmd, shell=True)  # nosec
