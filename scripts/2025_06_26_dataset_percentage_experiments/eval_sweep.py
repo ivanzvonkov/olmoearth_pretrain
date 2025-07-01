a3d8d701a7f114e15aec066fc92846bd67b00cdb
hgherzog <henryh@allenai.org>
2025-06-30 20:10:17 -0700

    run evals of what we have from dataset experiments

---
+++ scripts/2025_06_26_dataset_percentage_experiments/eval_sweep.py
@@ -0,0 +1,47 @@
+import argparse
+import subprocess
+
+LP_LRs = [1e-4, 5e-4, 1e-3, 5e-3, 1e-2, 5e-2, 1e-1, 5e-1]
+
+lr_args = " ".join(
+    [
+        "--trainer.callbacks.downstream_evaluator.tasks.m-cashew-plant.probe_lr={lr}",
+        "--trainer.callbacks.downstream_evaluator.tasks.mados.probe_lr={lr}",
+        "--trainer.callbacks.downstream_evaluator.tasks.sen1floods11.probe_lr={lr}",
+        "--trainer.callbacks.downstream_evaluator.tasks.pastis_sentinel2.probe_lr={lr}",
+        "--trainer.callbacks.downstream_evaluator.tasks.pastis_sentinel1.probe_lr={lr}",
+        "--trainer.callbacks.downstream_evaluator.tasks.pastis_sentinel1_sentinel2.probe_lr={lr}",
+        "--trainer.callbacks.downstream_evaluator.tasks.sickle_sentinel1.probe_lr={lr}",
+        "--trainer.callbacks.downstream_evaluator.tasks.sickle_landsat.probe_lr={lr}",
+        "--trainer.callbacks.downstream_evaluator.tasks.sickle_sentinel1_landsat.probe_lr={lr}",
+        "--trainer.callbacks.downstream_evaluator.tasks.breizhcrops.probe_lr={lr}",
+    ]
+)
+checkpoints = [
+    "weka/dfive-default/helios/checkpoints/henryh/latent_mim_cross_only_dec_wc_osm_srtm_dataset_percentage_sweep_1.0/step440000",
+    "weka/dfive-default/helios/checkpoints/henryh/latent_mim_cross_only_dec_wc_osm_srtm_dataset_percentage_sweep_0.5/step360000",
+    "weka/dfive-default/helios/checkpoints/henryh/latent_mim_cross_only_dec_wc_osm_srtm_dataset_percentage_sweep_0.25/step190000",
+    "weka/dfive-default/helios/checkpoints/henryh/latent_mim_cross_only_dec_wc_osm_srtm_dataset_percentage_sweep_0.125/step220000",
+    "weka/dfive-default/helios/checkpoints/henryh/latent_mim_cross_only_dec_wc_osm_srtm_dataset_percentage_sweep_0.0625/step100000",
+]
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--cluster", type=str, default="ai2/titan-cirrascale", help="Cluster to run on"
+)
+args = parser.parse_args()
+
+run_cmd = "launch"
+for checkpoint in checkpoints:
+    for probe_lr in LP_LRs:
+        run_name = (
+            checkpoint.split("/")[-2][20:]
+            + checkpoint.split("/")[-1]
+            + f"_eval_{probe_lr}"
+        )
+        start_command = "python3" if run_cmd == "launch" else "torchrun"
+        formatted_lr_args = lr_args.format(lr=probe_lr)
+        print(formatted_lr_args)
+        cmd = f"{start_command} scripts/2025_06_10_cross_masking_investigation/eval.py {run_cmd} {run_name} {args.cluster} --launch.priority=high {formatted_lr_args} --trainer.load_path={checkpoint} --launch.task_name=eval"
+        print(cmd)
+        subprocess.run(cmd, shell=True)  # nosec
