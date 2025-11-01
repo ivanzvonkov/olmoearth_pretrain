c56a21322ee57780a9dca2bd70f8b66076217c83
Yawen Zhang <yawenz@allenai.org>
2025-11-01 14:34:08 -0700

    debug why cluster is not taking effect

--- olmoearth_pretrain/internal/full_eval_sweep_finetune.py
+++ olmoearth_pretrain/internal/full_eval_sweep_finetune.py
@@ -277,21 +277,34 @@ def _format_launch_command(
     seed_args: Iterable[str],
 ) -> str:
     """Format the launch command."""
-    parts = [
-        f"TRAIN_SCRIPT_PATH={module_path}",
-        launch_command,
-        EVAL_LAUNCH_PATH,
-        sub_command,
-        run_name,
-        cluster,
-        "--launch.priority=urgent",
-        "--launch.num_gpus=1",
-        "--launch.preemptible=True",
-        "--launch.task_name=eval",
-        # Overwrite the max duration to enable eval of the last step of the checkpoint
-        "--trainer.max_duration.value=10000000",
-        "--trainer.max_duration.unit=steps",
-    ]
+    if sub_command == SubCmd.evaluate:
+        parts = [
+            f"TRAIN_SCRIPT_PATH={module_path}",
+            launch_command,
+            EVAL_LAUNCH_PATH,
+            sub_command,
+            run_name,
+            cluster,
+            # Overwrite the max duration to enable eval of the last step of the checkpoint
+            "--trainer.max_duration.value=10000000",
+            "--trainer.max_duration.unit=steps",
+        ]
+    else:
+        parts = [
+            f"TRAIN_SCRIPT_PATH={module_path}",
+            launch_command,
+            EVAL_LAUNCH_PATH,
+            sub_command,
+            run_name,
+            cluster,
+            "--launch.priority=urgent",
+            "--launch.num_gpus=1",
+            "--launch.preemptible=True",
+            "--launch.task_name=eval",
+            # Overwrite the max duration to enable eval of the last step of the checkpoint
+            "--trainer.max_duration.value=10000000",
+            "--trainer.max_duration.unit=steps",
+        ]
     parts.extend(checkpoint_args)
     parts.append(f"--trainer.callbacks.wandb.project={project_name}")
     parts.extend(extra_cli)
